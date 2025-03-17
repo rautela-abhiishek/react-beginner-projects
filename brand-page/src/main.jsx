aa2a28f3cbb06dc6783df4c2a287af13284c5958
rautela-abhiishek <abhisheksinghrautela72@gamil.com>
2025-03-17 23:05:49 +0530

    add first project in main repo

---
+++ brand-page/src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>Hello World</React.StrictMode>
+
+)
