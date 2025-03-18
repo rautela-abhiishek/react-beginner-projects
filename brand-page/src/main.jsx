efdfcd5ca21a9dcb0fee1f2cfc2de176035e5fe6
rautela-abhiishek <abhisheksinghrautela72@gamil.com>
2025-03-18 08:36:09 +0530

    add these files

--- brand-page/src/main.jsx
+++ brand-page/src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>Hello World</React.StrictMode>
-
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
